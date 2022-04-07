5165e51a9c766f0c57c6e3016631c32a3bf1fd00
Jakub Kliś <jakubklis2201@gmail.com>
2022-04-07 13:03:10 +0200

    BitMatrix

---
+++ BitMatrix/BitMatrix/BitMatrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitMatrix
+{
+    public class BitMatrix : IEnumerable<bool>
+    {
+        private BitArray[] data;
+
+        public int Dimension { get; }
+
+        public BitMatrix(int n)
+        {
+            data = new BitArray[n]; //tablica null-i
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = new BitArray(n);
+            }
+            Dimension = n;
+        }
+
+        public bool this[int i, int j]
+        {
+            get => data[i][j];
+            set => data[i][j] = value;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < Dimension; i++)
+            {
+                for (int j = 0; j < Dimension; j++)
+                {
+                    result += data[i][j] + " ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
+        public IEnumerator<bool> GetEnumerator()
+        {
+            foreach (var d in data)
+                foreach (bool x in d)
+                    yield return x;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ BitMatrix/BitMatrix/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BitMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BitMatrix m = new BitMatrix(4);
+            Console.WriteLine(m);
+            m[2, 3] = true;
+            Console.WriteLine(m[2,3]);
+            Console.WriteLine(m);
+        }
+    }
+}
