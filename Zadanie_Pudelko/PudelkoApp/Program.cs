ce37ee7be2d06c91a9170d50f85c503318c617c7
Jakub Kliś <jakubklis2201@gmail.com>
2022-04-30 13:16:07 +0200

    Koniec

--- Zadanie_Pudelko/PudelkoApp/Program.cs
+++ Zadanie_Pudelko/PudelkoApp/Program.cs
@@ -4,25 +4,16 @@ using System.Collections.Generic;
 
 namespace Zadanie_Pudelko
 {
-    /*
-    Funkcje oraz właściwości metod rozszerzających
-    - jest to metoda statyczna;
-    - musi być umieszczona w klasie statycznej;
-    - wykorzystuje słowo kluczowe this jako pierwszy parametr z typem zdefiniowanym w środowisku .NET;
-    */
-
     static class Program
     {
         public static Pudelko Kompresuj(this Pudelko p)
         {
-            double a = Math.Pow(p.Objetosc, (1 / 3D)); //D - decimal
+            double a = Math.Pow(p.Objetosc, (1 / 3D)); 
             return new Pudelko(a, a, a);
         }
 
         static void Main(string[] args)
         {
-            //W funkcji Main programu głównego (aplikacja konsolowa) utwórz listę kliku różnych pudełek, używając różnych wariantów konstruktora.
-            //Wypisz pudełka umieszczone na liście(po jednym w wierszu).
             List<Pudelko> pudelko = new List<Pudelko>();
             pudelko.Add(new Pudelko(2.5, 2.5, 2.5));
             pudelko.Add(new Pudelko(2.25, 2.5, 6.75));
@@ -40,7 +31,6 @@ namespace Zadanie_Pudelko
 
                 Console.WriteLine($"\t{pudelka} \t  [OBJĘTOŚĆ] {pudelka.Objetosc} || [POLE CAŁKOWITE] {pudelka.Pole} || [SUMA BOKÓW] {pudelka.Bok}");
             Console.WriteLine("\n\nPOSORTOWANE DANE\n");
-            // Posortuj tę listę według następującego kryterium: p1 poprzedza p2
 
             pudelko.Sort(delegate (Pudelko x, Pudelko y)
             {
