052b9533a177a377a2eead727ecc887d8044109f
Jakub Kliś <jakubklis2201@gmail.com>
2022-05-07 14:28:14 +0200

    Koniec

--- Hierarchia_klas_Pojazdy/Program.cs
+++ Hierarchia_klas_Pojazdy/Program.cs
@@ -10,15 +10,15 @@ namespace Vehicle
             List<Vehicle> Vehicles = new List<Vehicle>();
             LandVehicle bike = new LandVehicle("Bike",  FuelType.None, 0, 2);
             Vehicles.Add(bike);
-            AirVehicle plane = new AirVehicle("Mig",  FuelType.AviationFuel, 100, 3);
+            AirVehicle plane = new AirVehicle("F32",  FuelType.AviationFuel, 100, 3);
             Vehicles.Add(plane);
-            AmphibiousVehicle btr82 = new AmphibiousVehicle("Btr82", FuelType.Diesel, 720, 8, 10);
-            Vehicles.Add(btr82);
+            AmphibiousVehicle amfibia = new AmphibiousVehicle("Amfibia", FuelType.Diesel, 620, 6, 9);
+            Vehicles.Add(amfibia);
             NavalVehicle ship = new NavalVehicle("Ship", FuelType.Mazout, 40000, 5000);
             Vehicles.Add(ship);
             LandVehicle car = new LandVehicle("Car", FuelType.Gas, 110, 4);
             Vehicles.Add(car);
-            LandVehicle superCar = new LandVehicle("SuperCar", FuelType.Gas, 400, 4);
+            LandVehicle superCar = new LandVehicle("Bugatti", FuelType.Gas, 400, 4);
             Vehicles.Add(superCar);
 
             bike.Start();
@@ -29,8 +29,8 @@ namespace Vehicle
             plane.SwitchEnviroment(VehicleType.Land, VehicleType.Air);
             plane.Accelerate(100);
 
-            btr82.Start();
-            btr82.SwitchEnviroment(VehicleType.Land, VehicleType.Naval);
+            amfibia.Start();
+            amfibia.SwitchEnviroment(VehicleType.Land, VehicleType.Naval);
 
             ship.Start();
             ship.Accelerate(15);
